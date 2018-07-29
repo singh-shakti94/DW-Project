36255963cb6fd87257c98e25875d54e63bdf1c26
Shakti <cpt.shakti.singh@gmail.com>
2018-07-29 14:24:47 -0300

    new code added (comments and desc pending)

---
+++ classifier.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    print('Confusion matrix, without normalization')
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+# read cleaned data
+data = pd.read_csv("cleaned_data.csv")
+data = data.drop(["Unnamed: 0", "application_type", "zip_code", "addr_state", "earliest_cr_line",
+                  "issue_d", "id"], axis=1)
+
+# create one hot labels
+labels = pd.get_dummies(data=data["loan_status"])["Fully Paid"]
+data = data.drop("loan_status", axis=1)
+
+# convert categorical variables to numeric
+list_cat = ["term", "grade", "sub_grade", "emp_title", "emp_length", "home_ownership", "verification_status",
+            "purpose", "initial_list_status"]
+le = LabelEncoder()
+for i in list_cat:
+    print("Encoding labels ---")
+    le.fit(data[i])
+    data[i] = le.transform(data[i])
+
+# Normalizing the data and then training classifier
+x = data.values
+scaler = MinMaxScaler()
+data = pd.DataFrame(scaler.fit_transform(x))
+
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.33, random_state=42)
+
+
+def trainAndTest(X_train, X_test, y_train, y_test, clf_name="RF"):
+    if clf_name=="RF":
+        classifier = RandomForestClassifier()
+    elif clf_name=="LR":
+        classifier = LogisticRegression(penalty='l1', C=0.01)
+    elif clf_name=="NB":
+        classifier = BernoulliNB()
+    elif clf_name=="SVM":
+        classifier = SVC(kernel="linear")
+    classifier.fit(X=X_train, y=y_train)
+    predictions = classifier.predict(X=X_test)
+    print("Classification accuracy for %s :" % clf_name, accuracy_score(y_test, predictions))
+    cm = confusion_matrix(y_test, predictions)
+    plot_confusion_matrix(confusion_matrix(y_test, predictions),
+                          ["charged_off", "paid"])
+
+
+trainAndTest(X_train, X_test, y_train, y_test, clf_name="RF")
+trainAndTest(X_train, X_test, y_train, y_test, clf_name="LR")
+trainAndTest(X_train, X_test, y_train, y_test, clf_name="NB")
+
+# for svm use limited data
+sampled_data = data.sample(n=50000, random_state=42)
+sampled_labels = labels.sample(n=50000, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(sampled_data, sampled_labels, test_size=0.33, random_state=42)
+trainAndTest(X_train, X_test, y_train, y_test, clf_name="SVM")
+
+
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,18 +5,61 @@ import numpy as np
 raw_data = pd.read_csv("accepted_2007_to_2017.csv")
 
 # make a subset of data by randomly sampling (to work on it)
-subset = raw_data.sample(n=int(len(raw_data)/100))
+# subset = raw_data.sample(n=int(len(raw_data)/100))
 
 # pre-processing steps
 
 # 1 - find out null values
-subset.isnull().sum()
-for i in subset.columns:
-    if subset[i].isnull().sum() > (len(subset)/2):
+# raw_data.isnull().sum()
+raw_data.info(verbose=True, null_counts=True)
+
+for i in raw_data.columns:
+    if raw_data[i].isnull().sum() > (len(raw_data)/2):
         # drop columns that contains more than half null values
-        subset = subset.drop(i, axis=1)
-len(subset.columns)
+        raw_data = raw_data.drop(i, axis=1)
+len(raw_data.columns)
 
 # dropping columns that will be useless for EDA
-subset = subset.drop(["sub_grade", "pymnt_plan", "purpose", "zip_code",
-                      "addr_state"], axis=1)
+# subset = subset.drop(["sub_grade", "pymnt_plan", "purpose", "zip_code",
+#                       "addr_state"], axis=1)
+
+# lets take a look at the loan_status variable of the data
+raw_data["loan_status"].value_counts(dropna=False)
+
+# in the output of above code, we can see that there are many status levels, but
+# for this analysis we will be looking at loans that are either charged off or fully paid
+# so, we will be dripping the rows that have status different than "Charged off" or "Fully Paid"
+
+raw_data = raw_data.loc[raw_data["loan_status"].isin(["Fully Paid", "Charged Off"])]
+print("We are now left with %d rows" % len(raw_data))
+
+# check for NANs one more time
+raw_data.info(verbose=True, null_counts=True)
+# remove columns with NANs and columns that we don't need for further analysis
+raw_data = raw_data.drop(["next_pymnt_d", "debt_settlement_flag", "disbursement_method", "hardship_flag",
+                          "pymnt_plan", "title", ], axis=1)
+
+# take a look at the basic stats of the numerical valued columns
+raw_data.describe()
+
+# drop the columns that do not have any significant amount of data related to them
+# after going through every feature, i have compiled the following list of features to keep
+# although i may have missed some important ones, but that wii not put any significant effect on the analysis
+to_keep = ['addr_state', 'annual_inc', 'application_type', 'dti', 'earliest_cr_line', 'emp_length',
+             'emp_title', 'fico_range_high', 'fico_range_low', 'grade', 'home_ownership', 'id',
+             'initial_list_status', 'installment', 'int_rate', 'issue_d', 'loan_amnt', 'loan_status',
+             'mort_acc', 'open_acc', 'pub_rec', 'pub_rec_bankruptcies', 'purpose', 'revol_bal',
+             'revol_util', 'sub_grade', 'term', 'title', 'total_acc', 'verification_status', 'zip_code']
+
+to_drop = [feat for feat in raw_data.columns if feat not in to_keep]
+len(to_drop) # to be dropped
+
+raw_data = raw_data.drop(to_drop, axis=1)
+print("We are left with : ", raw_data.shape, "i.e %d rows(loans) and %d columns(features)" %(raw_data.shape))
+
+# get rid of NANs everywhere
+raw_data = raw_data.dropna()
+raw_data.info(verbose=True, null_counts=True)
+
+# this is the final data that we will be using for out analysis
+raw_data.to_csv("cleaned_data.csv")
